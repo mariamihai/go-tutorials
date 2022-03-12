cd75e7b627fa8c066d1751387dad47cc1c362cd6
maria <maria.ic.mihai@gmail.com>
2022-03-12 21:44:16 +0100

    [Add a test] Added tests for Hellos func, for both empty name and correct ones

--- 2-greetings-with-module/greetings/greetings_test.go
+++ 2-greetings-with-module/greetings/greetings_test.go
@@ -9,7 +9,7 @@ func TestHelloEmpty(t *testing.T) {
 	message, err := Hello("")
 
 	if message != "" || err == nil {
-		t.Fatalf(`Hello("") = %q, %v, want "", error`, message, err)
+		t.Fatalf(`Hello("") = [%q, %v], wants match for ["", error]`, message, err)
 	}
 }
 
@@ -20,6 +20,33 @@ func TestHelloName(t *testing.T) {
 	message, err := Hello(name)
 
 	if !want.MatchString(message) || err != nil {
-		t.Fatalf(`Hello(%q) = [%q, %v], want match for [%#q, nil]`, name, message, err, want)
+		t.Fatalf(`Hello(%q) = [%q, %v], wants match for [%#q, nil]`, name, message, err, want)
+	}
+}
+
+func TestHellosEmpty(t *testing.T) {
+	names := []string{"Go", ""}
+
+	messages, err := Hellos(names)
+
+	if messages != nil || err == nil {
+		t.Fatalf(`Hello(%q) = [%q, %v], wants match for ["", error]`, names, messages, err)
+	}
+}
+
+func TestHellosNames(t *testing.T) {
+	names := []string{"Go", "Other Name"}
+	messages, err := Hellos(names)
+
+	if messages == nil || err != nil {
+		t.Fatalf(`Hello(%q) = [%q, %v], wants match for ["", error]`, names, messages, err)
+	}
+
+	for _, name := range names {
+		want := regexp.MustCompile(`\b` + name + `\b`)
+
+		if !want.MatchString(messages[name]) || err != nil {
+			t.Fatalf(`Hello(%q) = [%q, %v], wants match for [%#q, nil]`, name, messages[name], err, want)
+		}
 	}
 }
