b51aed835021fe9c7f8d90cb615741369dbb3502
maria <maria.ic.mihai@gmail.com>
2022-03-12 18:15:24 +0100

    [Return multiple greetings] Added new function to return a map of names and greetings

--- 2-greetings-with-module/caller/call.go
+++ 2-greetings-with-module/caller/call.go
@@ -12,6 +12,7 @@ func main() {
 	// No timestamp, source file, line number
 	log.SetFlags(0)
 
+	// Get one greeting
 	message, err := greetings.Hello("Go")
 
 	if err != nil {
@@ -19,4 +20,13 @@ func main() {
 	}
 
 	fmt.Println(message)
+
+	// Get multiple greetings
+	messages, err := greetings.Hellos([]string{"Go", "Other Name", "Another Name"})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(messages)
 }
