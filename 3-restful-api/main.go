470e6e0288fbe4eff9a7b79b39b307fa525d9832
maria <maria.ic.mihai@gmail.com>
2022-03-15 19:55:37 +0100

    [RESTful API] Add web service with GET /albums endpoint

--- 3-restful-api/main.go
+++ 3-restful-api/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
 type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
@@ -11,4 +16,16 @@ var initAlbums = []album{
 	{ID: "1", Title: "Title 1", Artist: "Artist 1", Price: 10.12},
 	{ID: "2", Title: "Title 2", Artist: "Artist 2", Price: 14.48},
 	{ID: "3", Title: "Title 3", Artist: "Artist 1", Price: 9.56},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
+}
+
+func getAlbums(c *gin.Context) {
+	// can be replaced with Context.JSON to send a more compact JSON
+	c.IndentedJSON(http.StatusOK, initAlbums)
 }
