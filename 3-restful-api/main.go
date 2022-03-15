89980cae6ec797734cc8075d41150b12c9021e0b
maria <maria.ic.mihai@gmail.com>
2022-03-15 20:04:41 +0100

    [RESTful API] Add POST /albums endpoint

--- 3-restful-api/main.go
+++ 3-restful-api/main.go
@@ -12,7 +12,7 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
-var initAlbums = []album{
+var albums = []album{
 	{ID: "1", Title: "Title 1", Artist: "Artist 1", Price: 10.12},
 	{ID: "2", Title: "Title 2", Artist: "Artist 2", Price: 14.48},
 	{ID: "3", Title: "Title 3", Artist: "Artist 1", Price: 9.56},
@@ -21,11 +21,24 @@ var initAlbums = []album{
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
 
 func getAlbums(c *gin.Context) {
 	// can be replaced with Context.JSON to send a more compact JSON
-	c.IndentedJSON(http.StatusOK, initAlbums)
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Bind the received JSON to a new album
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
